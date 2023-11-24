f9a3e5414fac4280f562e8e9af7236feb4285877
extdevs <extdevs@kingkong.com.au>
2023-11-24 17:38:43 -0500

    updated axes

--- programacion_class/Ejercicios/tkinter_class/vistas/basic_calculator.py
+++ programacion_class/Ejercicios/tkinter_class/vistas/basic_calculator.py
@@ -1,6 +1,8 @@
 
 from tkinter import *
 from tkinter import messagebox as Messagebox
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 root = Tk()
 from controlador.suma_class import suma
 val_suma = suma()
@@ -8,24 +10,47 @@ val_suma = suma()
 class basic_frm():
 
     root.config(bg='steelblue')
-    root.geometry('600x400')
+    root.geometry('1300x550')
+    root.title('Basic Calculator')
     val_suma.center_window(root)
     
-    label1 = Label(root, text="Valor A.:   ", bg="steelblue", fg="White", font="Verdana, 15")
+    frame1 = Frame(root,borderwidth=2, bg='steelblue',width=500,height=300, relief="sunken")
+    frame1.grid(row=0,column=0, padx=50, pady=50)
+    
+    frame2 = Frame(root,borderwidth=2, bg='steelblue',width=500,height=300, relief="sunken")
+    frame2.grid(row=0,column=1, padx=50, pady=50, sticky="nsew")
+    
+    
+    parameters = {"xtick.labelsize": 5, "ytick.labelsize": 5, "axes.titlesize": 5}
+    plt.rcParams.update(parameters)
+    
+    fig, axs = plt.subplots()
+    fig.set_figheight(1.5)
+    fig.set_figwidth(2.5)
+    
+    axs.bar([1,2,3,23,24], [4,5,3,16,23])
+    axs.grid()
+    plt.title("Gráfica de barras")
+    
+    canvas = FigureCanvasTkAgg(fig, master=frame2)
+    canvas.draw()
+    canvas.get_tk_widget().grid(column=0, row=0)
+    
+    label1 = Label(frame1, text="Valor A.:   ", bg="steelblue", fg="White", font="Verdana, 15")
     label1.grid(row=0,column=0, padx=50, pady=10)
-    label2 = Label(root, text="Valor B.:   ", bg="steelblue", fg="White", font="Verdana, 15")
+    label2 = Label(frame1, text="Valor B.:   ", bg="steelblue", fg="White", font="Verdana, 15")
     label2.grid(row=1,column=0, padx=50, pady=10)
-    label3 = Label(root, text="Respuesta.: ", bg="steelblue", fg="White", font="Verdana, 15")
+    label3 = Label(frame1, text="Respuesta.: ", bg="steelblue", fg="White", font="Verdana, 15")
     label3.grid(row=2,column=0, padx=50, pady=10)
     
-    entry1 = Entry(root, justify=LEFT, borderwidth=2, textvariable=val_suma.valorA)
+    entry1 = Entry(frame1, justify=LEFT, borderwidth=2, textvariable=val_suma.valorA)
     entry1.grid(row=0,column=1, padx=10)
-    entry2 = Entry(root, justify=LEFT, borderwidth=2, textvariable=val_suma.valorB)
+    entry2 = Entry(frame1, justify=LEFT, borderwidth=2, textvariable=val_suma.valorB)
     entry2.grid(row=1,column=1, padx=10)
-    entry3 = Entry(root, justify=LEFT, borderwidth=2, textvariable=val_suma.respuesta)
+    entry3 = Entry(frame1, justify=LEFT, borderwidth=2, textvariable=val_suma.respuesta)
     entry3.grid(row=2,column=1, padx=10)
     
-    button1 = Button(root, text="Ejecutar", font="Verdana, 15", command=val_suma.operar)
+    button1 = Button(frame1, text="Ejecutar", font="Verdana, 15", command=val_suma.operar)
     button1.config(bg="steelblue")
     button1.grid(row=3, column=1, padx=10, pady=50)
     
@@ -36,7 +61,7 @@ class basic_frm():
     def advertencia(self):
         Messagebox.showinfo("Operación seleccionada...!", "Ha seleccionada la "+ val_suma.operacion.get())
     
-    btk = OptionMenu(root, val_suma.operacion, *options, command=advertencia)
+    btk = OptionMenu(frame1, val_suma.operacion, *options)
     btk.config(width=15, bg="steelblue")
     btk.grid(row=3, column=0, padx=50)
     
